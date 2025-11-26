7c022b02e8fdb9e6593f19a7a0d82463d1645e6a
strawberry-p <strawberry@example.com>
2025-11-27 00:15:40 +0100

    Fixed level loading, started working on level 2 To link a new level, attach levels.gd to the corresponding level select button in levels.tscn,  open its Inspector and set the lvl_load property to the path to your level

--- playphone/scripts/game.gd
+++ playphone/scripts/game.gd
@@ -1,10 +1,14 @@
 extends Node2D
-
+#var _new_scene_1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var _new_scene_1 = preload("res://Levels/Level1.tscn").instantiate()
-	$Level.set_scene("res://Levels/Level1.tscn")
+	if Globals.next_level == "":
+		#_new_scene_1 = preload("res://Levels/Level1.tscn").instantiate()
+		# what was that for?
+		print('uh oh when loading level')
+		Globals.next_level = "res://Levels/Level1.tscn"
+	$Level.set_scene(Globals.next_level)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
